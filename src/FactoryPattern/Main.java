11767bb81e885de05ff65244285ea251f7c2358d
Nguyen Tung Bach <bachntth2010055@fpt.edu.vn>
2022-03-29 21:16:54 +0700

    a

--- src/FactoryPattern/Main.java
+++ src/FactoryPattern/Main.java
@@ -1,12 +1,17 @@
 package FactoryPattern;
 
+import FactoryPattern.Entity.ICar;
 import FactoryPattern.View.CarFactory;
 
 public class Main {
     public static void main(String[] args) {
         CarFactory carFactory = new CarFactory();
-        carFactory.viewCar("HONDA");
-        carFactory.viewCar("NEXUS");
-        carFactory.viewCar("TOYOTA");
+        ICar car1= carFactory.getCar("HONDA");
+        ICar car2=carFactory.getCar("NEXUS");
+        ICar car3=carFactory.getCar("TOYOTA");
+
+        car1.view();
+        car2.view();
+        car3.view();
     }
 }
--- src/FactoryPattern/View/CarFactory.java
+++ src/FactoryPattern/View/CarFactory.java
@@ -6,17 +6,15 @@ import FactoryPattern.Entity.Brand.Nexus;
 import FactoryPattern.Entity.Brand.Toyota;
 
 public class CarFactory {
-    public void viewCar(String carType) {
-        ICar car;
+    public ICar getCar(String carType) {
+        ICar car = null;
         if (carType.equalsIgnoreCase("HONDA")) {
             car = new Honda();
-            car.view();
         } else if (carType.equalsIgnoreCase("NEXUS")) {
             car = new Nexus();
-            car.view();
         } else if (carType.equalsIgnoreCase("TOYOTA")) {
             car = new Toyota();
-            car.view();
         }
+        return car;
     }
 }
